d8ef186562031758a31b04c1eafe07dea226d7ee
Agustin Calderon <agustincl@gmail.com>
2014-02-11 18:59:09 +0100

    Funciones

---
+++ Funciones/matriz.php
@@ -0,0 +1,12 @@
+<?php
+$tabla = array(
+		array(1,2,3),
+		array(2,4,6),
+		array(3,6,9),
+);
+
+echo "<pre>";
+print_r($tabla);
+echo "</pre>";
+
+echo $tabla[2][1];
---
+++ Funciones/tabla_multiplicar.php
@@ -0,0 +1,40 @@
+
+<?php 
+function tabla_de_multiplicar ($a, $b)
+{	
+	for ($i=0;$i<=$b;$i++)
+	{	
+		for($m=0;$m<=$a;$m++)
+		{
+			if($i==0 && $m==0)
+				$tabla[$i][$m]="";		
+			else if($m==0)
+				$tabla[$i][$m]=$i;			
+			else if ($i==0)	
+				$tabla[$i][$m]=$m;
+			else 
+				$tabla[$i][$m]=$m*$i;
+		}
+	}
+	return $tabla;
+}
+
+
+
+$a=5;
+$b=4;
+
+$tabla=tabla_de_multiplicar ($a, $b);
+echo "<pre>";
+print_r($tabla);
+echo "</pre>";
+
+
+
+
+
+
+
+
+
+
