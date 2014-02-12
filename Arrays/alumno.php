0f21a78effdb5a4a9e4499ea2ba7bed89e23cfbe
Agustin Calderon <agustincl@gmail.com>
2014-02-12 18:21:12 +0100

    Dates

--- Arrays/alumno.php
+++ Arrays/alumno.php
@@ -1,1 +1,19 @@
 <?php
+$alumno = array(
+		'id'=>0,
+		'nombre'=>'Agustin',
+		'apellido'=>'Calderon',
+		'email'=>'agustincl@gmail.com',
+		'password'=>'1234',
+		'edad'=>'3',
+		'mascotas'=>'gato',
+		'idiomas'=>array('english','español'),
+		'descripcion'=>'esto es la descripcion',
+		'foto'=>'/images/agustin.jpg',
+		'ciudad'=>'santiago',
+		'sexo'=>'otros'
+);
+
+echo "<pre>";
+print_r($alumno);
+echo "</pre>";
---
+++ Fechas/hoy.php
@@ -0,0 +1,9 @@
+
+
+Hoy estamos en la semana 6 del año 2014. 
+Es miercoles de febrero, dia 12.
+<hr/>
+<?php
+echo date("\H\o\y \\e\s\\t\a\m\o\s \\e\\n \\l\a 
+		\s\\e\m\a\\n\a W \d\\e\l \a\ñ\o Y\. 
+		\E\s l \d\\e F\\, \d\i\a j\.");
