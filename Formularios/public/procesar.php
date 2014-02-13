d6951329b4ab44f87c6303b5abe5cdf6abba1380
Agustin Calderon <agustincl@gmail.com>
2014-02-13 19:12:14 +0100

    Formularios, estructura

--- Formularios/public/procesar.php
+++ Formularios/public/procesar.php
@@ -43,22 +43,26 @@ $_POST[]=$fichero;
 move_uploaded_file($_FILES['photo']['tmp_name'], 
 					$destino."/".$fichero);
 
+$array_salida=array();
+// Para cada elemento del array POST
+foreach($_POST as $value)
+{
+	// Si no es un array
+	if(!is_array($value)) 
+		// enviar a un array de salida
+		$array_salida[]=$value;
+	// Si es un array
+	else	
+		// Dividir por pipes y
+		// enviar a un array de salida
+		$array_salida[]=implode('|',$value);		
+}
 
 // Separar por comas los datos
-$data=implode(',',$_POST);
-// Salto de linea
-$data.="\n";
+$data=implode(',',$array_salida);
 
+// Agregar salto de linea
+$data.="\n";
 
 // Estibir datos en un archivo de texto
 file_put_contents('alumnos.txt', $data, FILE_APPEND);
-
-
-
-
-
-
-
-
-
-
