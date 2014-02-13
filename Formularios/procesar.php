411d30c2105e9d3fa2ea225ce8d500c7f9aa962e
Agustin Calderon <agustincl@gmail.com>
2014-02-13 17:55:17 +0100

    Subir foto con cambio de nombre

--- Formularios/procesar.php
+++ Formularios/procesar.php
@@ -1,5 +1,5 @@
 <?php
-
+// Debug
 echo "<pre>Get: ";
 print_r($_GET);
 echo "</pre>";
@@ -12,10 +12,53 @@ echo "<pre>Files: ";
 print_r($_FILES);
 echo "</pre>";
 
+
+//Tomar el destino
+$destino = $_SERVER['DOCUMENT_ROOT'];
+
+//Tomar el nombre del fichero
+$fichero = $_FILES['photo']['name'];
+
+// Datos del fichero
+$datafichero = pathinfo($destino."/".$fichero);
+
+$a=0;
+//Mientras que el fichero exista en el destino
+while (file_exists($destino."/".$fichero))	// hasta que no exista
+{
+	//aumentar la variable 
+	$a++;
+	//concatenarsela al nombre del fichero
+	$fichero = $datafichero['filename'].
+				"_".
+				$a.
+				".".
+				$datafichero['extension'];
+}
+
+// Agregar informacion de la foto al final del POST
+$_POST[]=$fichero;
+
+// Subir foto
+move_uploaded_file($_FILES['photo']['tmp_name'], 
+					$destino."/".$fichero);
+
+
+// Separar por comas los datos
 $data=implode(',',$_POST);
+// Salto de linea
 $data.="\n";
 
+
+// Estibir datos en un archivo de texto
 file_put_contents('alumnos.txt', $data, FILE_APPEND);
-move_uploaded_file($_FILES['photo']['tmp_name'], 
-					$_SERVER['DOCUMENT_ROOT']."/".
-					$_FILES['photo']['name']);
+
+
+
+
+
+
+
+
+
+
